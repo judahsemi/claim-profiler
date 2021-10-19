74e34913c2463909e750a602f926cbcaff04b505
Banjoko Judah <banjokojudah@gmail.com>
2021-10-19 05:18:01 +0100

    Add minimum working web interface

--- web/main/settings.py
+++ web/main/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # local apps
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
